07e3aaf0231b422133170544c83e9dfdec9f1379
fatihcaliss <fthcls@gmail.com>
2023-12-19 18:02:59 +0300

    add env for apiKey

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyA_dcJfSrTESepPnH62aV1jhiLoR2M620w";
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
