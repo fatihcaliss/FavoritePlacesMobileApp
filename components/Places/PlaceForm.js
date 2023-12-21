9e74f5ecf8c12735526d2ce60b65880d948deafe
fatihcaliss <fthcls@gmail.com>
2023-12-21 23:14:54 +0300

    output a list of places

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -5,8 +5,9 @@ import { Colors } from "../../constants/colors";
 import Button from "../UI/Button";
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
+import { Place } from "../../models/place";
 
-function PlaceForm() {
+function PlaceForm({ onCreatePlace }) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [selectedImage, setSelectedImage] = useState();
   const [pickedLocation, setPickedLocation] = useState();
@@ -24,9 +25,11 @@ function PlaceForm() {
   }, []);
 
   function savePlaceHandler() {
-    console.log(enteredTitle);
-    console.log(selectedImage);
-    console.log(pickedLocation);
+    const placeData = new Place(enteredTitle, selectedImage, pickedLocation);
+    onCreatePlace(placeData);
+    // console.log(enteredTitle);
+    // console.log(selectedImage);
+    // console.log(pickedLocation);
   }
 
   return (
